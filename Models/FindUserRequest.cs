bcad1918e691d201b775d4f7183ef32bff45388c
CharaD7 <jijakahn6@gmail.com>
2025-06-19 13:47:28 +0000

    Defined FindUserRequest Model

---
+++ Models/FindUserRequest.cs
@@ -0,0 +1,6 @@
+public class FindUserRequest
+{
+    public required string AccessToken { get; set; }
+    public required string idNumber { get; set; }
+    public required string idType { get; set;  }
+}
